9c1dffe01b0dc594f17f2facb67ff70b59e8902d
Dragner8 <piotrgrap@gmail.com>
2016-12-06 00:20:52 +0100

    Create Migration.java

---
+++ src/Topology/Migration.java
@@ -0,0 +1,24 @@
+package Topology;
+
+
+
+import java.util.List;
+
+public class Migration
+{
+    private int from;
+    private int where;
+    private List<Creature> creatures;
+
+    public Migration(int where, int from, List<Creature> creatures)
+    {
+        this.where = where;
+        this.from = from;
+        for(Creature island : creatures) this.creatures.add(creature);
+    }
+
+    public void doMigration()
+    {
+        
+    }
+}
