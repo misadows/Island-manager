c30e2083e9f468b11f062e19b8568be993f46dbb
gosiaol <gosiek_ol@wp.pl>
2017-01-06 18:52:56 +0100

    Add ChartsController, RootLayoutController, Charts.fxml file, changed MainApp

--- src/Visualisation/MainApp.java
+++ src/Visualisation/MainApp.java
@@ -1,17 +1,22 @@
 package Visualisation;
 
+import Island.EpochResult;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.Modality;
+
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainApp extends Application {
     private Stage primaryStage;
     private BorderPane rootLayout;
+    ArrayList<EpochResult> ep;
 
     public MainApp() {
         //Initialization some sample data
@@ -64,6 +69,29 @@ public class MainApp extends Application {
         }
 
 
+    }
+    public void showCharts() {
+        try {
+            // Load the fxml file and create a new stage for the popup
+            FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("Charts.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("CHART");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the island into the controller
+            ChartsController controller = loader.getController();
+            controller.setResultsData(ep);
+
+            dialogStage.show();
+
+        } catch (IOException e) {
+            // Exception gets thrown if the fxml file could not be loaded
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) { launch(args); }
